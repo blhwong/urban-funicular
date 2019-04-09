c634793312478f235dba3f9ad0c4c511f732807b
Brandon Wong <brandon@yerdle.com>
2019-04-08 21:04:02 -0700

    Add tests for ship model

--- models/ship.js
+++ models/ship.js
@@ -1,5 +1,27 @@
 class Ship {
   constructor(row, col, size, direction) {
+    const errors = [];
+    if (typeof row !== 'number') {
+      errors.push('Expect number to input row');
+    }
+    if (typeof col !== 'number') {
+      errors.push('Expect number to input column');
+    }
+    if (typeof size !== 'number') {
+      errors.push('Expect number to input size');
+    }
+    if (size > 1 && (direction !== 'vertical' && direction !== 'horizontal')) {
+      errors.push('Direction must be vertical or horizontal');
+    }
+    if (row < 0) {
+      errors.push('Row out of bounds');
+    }
+    if (col < 0) {
+      errors.push('Column out of bounds');
+    }
+    if (errors.length > 0) {
+      throw new Error(errors.join('. '));
+    }
     this.row = row;
     this.col = col;
     this.size = size;
@@ -8,6 +30,9 @@ class Ship {
   }
 
   hit() {
+    if (this.hits >= this.size) {
+      throw new Error('Ship has already sunk');
+    }
     this.hits += 1;
   }
 
---
+++ test/models/ship.js
@@ -0,0 +1,55 @@
+const { expect } = require('chai');
+const Ship = require('../../models/ship');
+
+describe('Ship model', () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(4, 3, 2, 'horizontal');
+  });
+
+  describe('constructor', () => {
+    it('can create a ship', () => {
+      expect(ship.row).to.equal(4);
+      expect(ship.col).to.equal(3);
+      expect(ship.size).to.equal(2);
+      expect(ship.direction).to.equal('horizontal');
+      expect(ship.hits).to.equal(0);
+    });
+
+    it('does not expect direction for ships at size 1', () => {
+      expect(() => new Ship(1, 1, 1)).not.to.throw();
+      expect(() => new Ship(1, 1, 2)).to.throw();
+      expect(() => new Ship(1, 1, 2, 'blah')).to.throw();
+      expect(() => new Ship(1, 1, 2, 'horizontal')).not.to.throw();
+      expect(() => new Ship(1, 1, 2, 'vertical')).not.to.throw();
+    });
+
+    it('will throw error on bad inputs', () => {
+      expect(() => new Ship(null, undefined, '1', 'test')).to.throw();
+      expect(() => new Ship(-1, -1, '1', 'test')).to.throw();
+    });
+  });
+
+  describe('Ship.hit', () => {
+    it('can increment hit counter', () => {
+      ship.hit();
+      expect(ship.hits).to.equal(1);
+    });
+
+    it('will throw an error if no more hits left', () => {
+      ship.hit();
+      ship.hit();
+      expect(() => ship.hit()).to.throw();
+    });
+  });
+
+  describe('Ship.isSunk', () => {
+    it('can determine if sunk', () => {
+      expect(ship.isSunk()).to.equal(false);
+      ship.hit();
+      ship.hit();
+      expect(ship.isSunk()).to.equal(true);
+    });
+  });
+});
